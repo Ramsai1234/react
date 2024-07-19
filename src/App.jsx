d552aea5418f3d832015f8b4269c4fd4de2c4b98
P.Ramsai <ponugotiramsai@gmail.com>
2024-07-19 14:18:50 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Stopwatch from './Components/stopwatch';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Simple Stopwatch</h1>
+      <Stopwatch />
+    </div>
+  );
+}
+
+export default App;
